ab36dd2e206dea430f9dddba8d486402b5e3b956
Tomáš Wróbel <coder@tomaswrobel.dev>
2023-07-14 13:16:54 +0200

    Config edit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,1 @@
-module.exports = require("@next/mdx")()({
-    /**
-     * @param {import("webpack").Configuration} config
-     * @returns
-     */
-    webpack(config) {
-        config.resolve.extensions.push("...");
-        return config;
-    },
-});
+module.exports = require("@next/mdx")()();
