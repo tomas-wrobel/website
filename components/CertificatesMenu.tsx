3418d473310fc8127249222bcbd133d4388bcd82
Tomáš <tomas.wrobel@email.cz>
2023-09-09 15:19:56 +0200

    Background removal from "code.png"

--- components/CertificatesMenu.tsx
+++ components/CertificatesMenu.tsx
@@ -1,36 +1,34 @@
 import Link from "next/link";
 import type {FunctionComponent} from "react";
 
-const CertificatesMenu: FunctionComponent = () => {
-    return (
-        <ul className="nav nav-menu" id="pp-menu">
-            <li>
-                <Link href="/" className="nav-link">
-                    <span>Home</span>
-                </Link>
-            </li>
-            <li>
-                <Link href="/#about" className="nav-link">
-                    <span>About Me</span>
-                </Link>
-            </li>
-            <li>
-                <Link href="/#services" className="nav-link">
-                    <span>Services</span>
-                </Link>
-            </li>
-            <li>
-                <Link href="/#blog" className="nav-link">
-                    <span>Blog</span>
-                </Link>
-            </li>
-            <li>
-                <Link href="/#contactus" className="nav-link">
-                    <span>Contact Me</span>
-                </Link>
-            </li>
-        </ul>
-    );
-};
+const CertificatesMenu: FunctionComponent = () => (
+	<ul className="nav nav-menu" id="pp-menu">
+		<li>
+			<Link href="/" className="nav-link">
+				<span>Home</span>
+			</Link>
+		</li>
+		<li>
+			<Link href="/#about" className="nav-link">
+				<span>About Me</span>
+			</Link>
+		</li>
+		<li>
+			<Link href="/#services" className="nav-link">
+				<span>Services</span>
+			</Link>
+		</li>
+		<li>
+			<Link href="/#blog" className="nav-link">
+				<span>Blog</span>
+			</Link>
+		</li>
+		<li>
+			<Link href="/#contactus" className="nav-link">
+				<span>Contact Me</span>
+			</Link>
+		</li>
+	</ul>
+);
 
 export default CertificatesMenu;
