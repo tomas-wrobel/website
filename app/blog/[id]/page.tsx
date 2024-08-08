25b9b07574d49871477de381c8e5ffbec8221ce7
Tomáš Wróbel <tomas.wrobel@email.cz>
2024-08-08 18:23:40 +0200

    Blog layout

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,6 +1,6 @@
 import "highlight.js/scss/monokai-sublime.scss";
-import DayNight from "../../components/DayNight";
 import Link from "next/link";
+import Page from "../../components/Page";
 
 export interface Props {
     params: {
@@ -11,7 +11,7 @@ export interface Props {
 export async function generateStaticParams() {
     const {readdir} = await import("fs/promises");
     const array = await readdir("app/blog/posts");
-    return array.map(id => ({id}));
+    return array.map(id => ({ id }));
 }
 
 export async function generateMetadata({params}: Props) {
@@ -28,46 +28,47 @@ export default async function BlogPage({params}: Props) {
 
     return (
         <>
-            <DayNight />
             <Link className="demo-back-link" href="/">
                 <i className="fas fa-arrow-left" />
             </Link>
-            <div className="single-blog">
-                <div className="container">
-                    <div className="blog-feature-img">
-                        <img src={post.img} title="" alt="" />
-                    </div>
-                    <div className="row justify-content-center">
-                        <div className="col-lg-8">
-                            <article className="article">
-                                <div className="article-title">
-                                    <h2>{post.title}</h2>
-                                    <div className="media">
-                                        <div className="avatar">
-                                            <img
-                                                src="/img/me.png"
-                                                title=""
-                                                alt=""
-                                            />
-                                        </div>
-                                        <div className="media-body">
-                                            <label>Tomáš Wróbel</label>
-                                            <span>
-                                                {new Date(
-                                                    post.date
-                                                ).toLocaleDateString("cs")}
-                                            </span>
+            <Page current="blog">
+                <div className="single-blog">
+                    <div className="container">
+                        <div className="blog-feature-img">
+                            <img src={post.img} title="" alt="" />
+                        </div>
+                        <div className="row justify-content-center">
+                            <div className="col-lg-8">
+                                <article className="article">
+                                    <div className="article-title">
+                                        <h2>{post.title}</h2>
+                                        <div className="media">
+                                            <div className="avatar">
+                                                <img
+                                                    src="/img/me.png"
+                                                    title=""
+                                                    alt=""
+                                                />
+                                            </div>
+                                            <div className="media-body">
+                                                <label>Tomáš Wróbel</label>
+                                                <span>
+                                                    {new Date(
+                                                        post.date
+                                                    ).toLocaleDateString("cs")}
+                                                </span>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div className="article-content">
-                                    <post.default />
-                                </div>
-                            </article>
+                                    <div className="article-content">
+                                        <post.default />
+                                    </div>
+                                </article>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </Page>
         </>
     );
 }
