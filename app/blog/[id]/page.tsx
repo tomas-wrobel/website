62fd56e5cb43d5f7277b62f1fc9239ebfbc062fa
Tomáš Wróbel <tomas.wrobel@email.cz>
2024-08-08 18:26:29 +0200

    Removing unnecessary back button

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,5 +1,4 @@
 import "highlight.js/scss/monokai-sublime.scss";
-import Link from "next/link";
 import Page from "../../components/Page";
 
 export interface Props {
@@ -11,7 +10,7 @@ export interface Props {
 export async function generateStaticParams() {
     const {readdir} = await import("fs/promises");
     const array = await readdir("app/blog/posts");
-    return array.map(id => ({ id }));
+    return array.map(id => ({id}));
 }
 
 export async function generateMetadata({params}: Props) {
@@ -27,48 +26,43 @@ export default async function BlogPage({params}: Props) {
     const post = await import(`../posts/${params.id}`);
 
     return (
-        <>
-            <Link className="demo-back-link" href="/">
-                <i className="fas fa-arrow-left" />
-            </Link>
-            <Page current="blog">
-                <div className="single-blog">
-                    <div className="container">
-                        <div className="blog-feature-img">
-                            <img src={post.img} title="" alt="" />
-                        </div>
-                        <div className="row justify-content-center">
-                            <div className="col-lg-8">
-                                <article className="article">
-                                    <div className="article-title">
-                                        <h2>{post.title}</h2>
-                                        <div className="media">
-                                            <div className="avatar">
-                                                <img
-                                                    src="/img/me.png"
-                                                    title=""
-                                                    alt=""
-                                                />
-                                            </div>
-                                            <div className="media-body">
-                                                <label>Tomáš Wróbel</label>
-                                                <span>
-                                                    {new Date(
-                                                        post.date
-                                                    ).toLocaleDateString("cs")}
-                                                </span>
-                                            </div>
+        <Page current="blog">
+            <div className="single-blog">
+                <div className="container">
+                    <div className="blog-feature-img">
+                        <img src={post.img} title="" alt="" />
+                    </div>
+                    <div className="row justify-content-center">
+                        <div className="col-lg-8">
+                            <article className="article">
+                                <div className="article-title">
+                                    <h2>{post.title}</h2>
+                                    <div className="media">
+                                        <div className="avatar">
+                                            <img
+                                                src="/img/me.png"
+                                                title=""
+                                                alt=""
+                                            />
+                                        </div>
+                                        <div className="media-body">
+                                            <label>Tomáš Wróbel</label>
+                                            <span>
+                                                {new Date(
+                                                    post.date
+                                                ).toLocaleDateString("cs")}
+                                            </span>
                                         </div>
                                     </div>
-                                    <div className="article-content">
-                                        <post.default />
-                                    </div>
-                                </article>
-                            </div>
+                                </div>
+                                <div className="article-content">
+                                    <post.default />
+                                </div>
+                            </article>
                         </div>
                     </div>
                 </div>
-            </Page>
-        </>
+            </div>
+        </Page>
     );
 }
